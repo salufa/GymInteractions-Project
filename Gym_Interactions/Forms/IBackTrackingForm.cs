ca53ab583c22c2db1f35f9b0b0b62ae769046c5b
George Christidis <s4luf4@hotmail.com>
2016-01-08 17:11:48 +0200

    8-1-26 commit concerning a pull error

---
+++ Gym_Interactions/Forms/IBackTrackingForm.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Gym_Interactions.Forms
+{
+    public interface IBackTrackingForm
+    {
+        void Visit(Form from);
+        void Backtrack();
+    }
+}
